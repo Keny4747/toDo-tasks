ba75aaf07bc724f203d6e7e4e2870d8018ead92f
Keny Pau Vas <keny_pauccarima@usmp.pe>
2022-11-15 10:52:34 -0500

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'toDo';
+
+
+  habilitar : boolean =true;
+
+  arrToDos: string[] = [
+    'Estudiar 2 horas',
+    'Tomar un descanso de 30 minutos',
+    'Poner en practica lo aprendido',
+  ];
+
+  toDo: string = '';
+  classLi: string = '';
+  finish: boolean = false;
+
+  addToDos(argumentToDo: string) {
+    this.arrToDos.push(argumentToDo);
+    this.toDo = '';
+
+    console.log(
+      'Si lograste cambiar el bg de html o body ayudame =(, mi twitter es : MooenzDev'
+    );
+  }
+
+  deleteToDo(indice:any) {
+    this.arrToDos.splice(indice, 1);
+  }
+
+  finishToDo(indice: number) {
+    const indiceStr = indice.toString();
+    const element = document.querySelector(`#id${indiceStr}`);
+    element!.classList.toggle('terminado');
+  }
 }
